8a5d97020ab2614484833103f0c6b0c525cfa1a5
Jia <jovsofjdo@gmail.com>
2023-08-25 10:23:31 -0700

    Fixed home page route

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const app = express();
 app.use(cors());
 
 //sets the path to my dirctory
-const _jiadirname = path.resolve();
+const jiadirname = path.resolve();
+//console.log(_jiadirname)
 
-const PORT = 8080;
+const PORT = 8080; 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_jiadirname, 'index.html'))
+    console.log('someone is home')
+    res.sendFile(path.join(jiadirname, 'index.html'));
 })
+
+app.listen(PORT, () => {
+    console.log(`Hi Barbie! Server is running on port ${PORT}`)
+});
